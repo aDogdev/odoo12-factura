a49c85b22488046dade8bd298cbf6419590ee611
aDog <adrianmg3008@gmail.com>
2024-03-28 10:26:23 -0400

    fix invoice_validate implementation

--- models/factura.py
+++ models/factura.py
@@ -11,21 +11,6 @@ class Factura(models.Model):
 
     @api.multi
     def invoice_validate(self):
-        for invoice in self:
-            if invoice.partner_id not in invoice.message_partner_ids:
-                invoice.message_subscribe([invoice.partner_id.id])
-
-            # Almacenar el usuario actual
-            invoice.user_validated = self.env.user
-
-            # Auto-compute reference, if not already existing and if configured on company
-            if not invoice.reference and invoice.type == 'out_invoice':
-                invoice.reference = invoice._get_computed_reference()
-
-            # DO NOT FORWARD-PORT.
-            # The reference is copied after the move creation because we need the move to get the invoice number but
-            # we need the invoice number to get the reference.
-            invoice.move_id.ref = invoice.reference
-        self._check_duplicate_supplier_reference()
-
-        return self.write({'state': 'open'})
+        res = super().invoice_validate()            
+        self.write({"user_validated": self.env.user.id}) 
+        return res
