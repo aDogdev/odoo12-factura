776e7a50d0e5b963bb1992c4bd9548dc1b6786d7
aDog <adrianmg3008@gmail.com>
2024-04-02 13:29:31 -0400

    add diferenciar facturas originales  y facturas rectificativas del cliente en una misma lista

--- models/factura.py
+++ models/factura.py
@@ -8,6 +8,16 @@ class Factura(models.Model):
     _description = 'Factura'
 
     user_validated = fields.Many2one('res.users', readonly=True, string='Usuario que validó')
+    
+    rectification = fields.Selection([('rectificativa','Rectificativa'),('original','Original')], compute='_compute_rectification')
+
+    @api.depends('refund_invoice_id')
+    def _compute_rectification(self):
+        for invoice in self:
+            if bool(invoice.refund_invoice_id):
+                invoice.rectification = "rectificativa"
+            else: 
+                invoice.rectification = "original"
 
     @api.multi
     def invoice_validate(self):
